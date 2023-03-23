2de250e201d130eddd5cb46eec850c9e497f435f
kman2022 <116604620+kman2022@users.noreply.github.com>
2023-03-23 22:45:11 +0100

    learn

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ LOC_PATH = '/Users/kristian.lande@leveltenenergy.com/Documents/GitHub/streamlit-
 UC_IMAGE = 'berkley_logo.png'
 PWR_IMAGE = 'power_pic.jpeg'
 
-st.set_page_config(page_title="PJM Interconnection Dashboard",
+st.set_page_config(page_title="Interconnection Dashboard",
                    page_icon=LOC_PATH+UC_IMAGE,
                    layout="wide")
 
@@ -14,18 +14,14 @@ src_markdown = """
 **[Berkley Lab Reports](https://emp.lbl.gov/)**  
 GitHub Repository: <https://github.com/kman2022/streamlit-multipage-template>
 """
-src_analysis = """
-    **Source:** Berkley Lab Queued Up v2: Extended Analysis on Power Plants Seeking Transmission InterconnectionAs of the End of 2021. Joseph Rand, Will Gorman, Dev Millstein, Andrew Mills, Joachim Seel, Ryan Wiser Lawrence Berkeley National Laboratory February 2022.  <https://emp.lbl.gov/publications/queued-characteristics-power-plants>
-"""
 
-st.sidebar.info(src_analysis)
 st.sidebar.title("About")
 st.sidebar.info(src_markdown)
 logo = "https://emp.lbl.gov/sites/all/files/logo.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Queued Up  ⚡ ")
+st.title("Berkley Labs Data")
 
 st.markdown(
     """
@@ -36,7 +32,7 @@ st.image(LOC_PATH+PWR_IMAGE,width=350)
 
 st.header("Instructions")
 
-markdown = """
+instructions = """
 1. For the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template) or [use it as a template](https://github.com/giswqs/streamlit-multipage-template/generate) for your own project.
 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
 3. Find your favorite emoji from https://emojipedia.org.
@@ -44,7 +40,7 @@ markdown = """
 
 """
 
-st.markdown(markdown)
+st.markdown(instructions)
 
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("OpenTopoMap")
